ff59502b4dd70bd631a48c2565e1eab9e94f3f82
Pranav <pranavkurup26@gmail.com>
2024-10-11 08:38:23 -0700

    rotation_fixed

--- GeneralScripts/CameraWork/dalton.gd
+++ GeneralScripts/CameraWork/dalton.gd
@@ -1,10 +1,8 @@
 extends CharacterBody3D
 
-
+@onready var armature = %DetectiveT7
 const SPEED = 5.0
-var _last_movement_direction := Vector3.BACK
-
-
+const LERP_VAL = .15
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -13,16 +11,20 @@ func _physics_process(delta: float) -> void:
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("Left", "Right", "Forward", "Back")
+	var input_dir := Input.get_vector("Right", "Left", "Back", "Forward")
 	var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
-		velocity.x = -direction.x * SPEED
-		velocity.z = direction.y * SPEED
+		velocity.x = direction.x * SPEED
+		velocity.z = direction.z * SPEED
+		armature.rotation.y = lerp_angle(armature.rotation.y, atan2(velocity.x, velocity.z), LERP_VAL)
+
 	else:
-		velocity.x = move_toward(-velocity.x, 0, SPEED)
+		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+	
+
 	
 		
 		
