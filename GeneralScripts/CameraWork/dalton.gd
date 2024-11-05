b0773ffe1ff7a0cd6f12ef1ab5b4467721c88d7a
Pranav <pranavkurup26@gmail.com>
2024-11-05 15:34:21 -0800

    Direction of movement now changes with cam

--- GeneralScripts/CameraWork/dalton.gd
+++ GeneralScripts/CameraWork/dalton.gd
@@ -5,7 +5,7 @@ extends CharacterBody3D
 @onready var pos_x = 0
 @onready var pos_y = 0
 @onready var marker = $Marker2D
-@onready var camera = $"../../SubViewportContainer/SubViewport/CameraSystem/Camera3D"
+@export var camera = Camera3D
 const SPEED = 1.15
 const LERP_VAL = .15
 
@@ -21,12 +21,17 @@ func _physics_process(delta: float) -> void:
 		# Get the input direction and handle the movement/deceleration.
 		# As good practice, you should replace UI actions with custom gameplay actions.
 		var input_dir := Input.get_vector("Right", "Left", "Back", "Forward")
-		var direction := (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-		if direction:
-			velocity.x = lerp(velocity.x, direction.x * SPEED, LERP_VAL)
-			velocity.z = lerp(velocity.z, direction.z * SPEED, LERP_VAL)
-			armature.rotation.y = lerp_angle(armature.rotation.y, atan2(velocity.x, velocity.z), LERP_VAL)
+		if input_dir != Vector2.ZERO:
+			# Rotate input direction based on the camera's orientation
+			var camera_basis = camera.transform.basis
+			var rotated_dir = (camera_basis.x * input_dir.x + camera_basis.z * input_dir.y).normalized()
+			
+			# Set movement direction and apply smooth movement
+			velocity.x = lerp(velocity.x, -rotated_dir.x * SPEED, LERP_VAL)
+			velocity.z = lerp(velocity.z, -rotated_dir.z * SPEED, LERP_VAL)
 
+			# Smoothly rotate the armature to face the movement direction
+			armature.rotation.y = lerp_angle(armature.rotation.y, atan2(-rotated_dir.x, -rotated_dir.z), LERP_VAL)
 		else:
 			velocity.x = lerp(velocity.x, 0.0, LERP_VAL)
 			velocity.z = lerp(velocity.z, 0.0, LERP_VAL)
