5778518d513b3edcd9e172791a0a5167d6d61800
Pranav <pranavkurup26@gmail.com>
2024-11-06 18:17:44 -0800

    scene changes and second house movement

--- GeneralScripts/CameraWork/camera_system.gd
+++ GeneralScripts/CameraWork/camera_system.gd
@@ -1,75 +1,54 @@
 extends Node
 
-@export var cam1: Node3D
-@export var cam2: Node3D
-@export var cam3: Node3D
-@export var cam4: Node3D
-@export var cam5: Node3D
+@export var cameras: Array[Node3D]
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-func cam1_on():
-	cam1.set_priority(4)
-	cam2.set_priority(3)
-	cam3.set_priority(2)
-	cam4.set_priority(1)
-	cam5.set_priority(0)
-	
-func cam2_on():
-	cam1.set_priority(3)
-	cam2.set_priority(4)
-	cam3.set_priority(2)
-	cam4.set_priority(1)
-	cam5.set_priority(0)
-	
-func cam3_on():
-	cam1.set_priority(3)
-	cam2.set_priority(2)
-	cam3.set_priority(4)
-	cam4.set_priority(1)
-	cam5.set_priority(0)
-	
-func cam4_on():
-	cam1.set_priority(3)
-	cam2.set_priority(2)
-	cam3.set_priority(1)
-	cam4.set_priority(4)
-	cam5.set_priority(0)
-	
-func cam5_on():
-	cam1.set_priority(3)
-	cam2.set_priority(2)
-	cam3.set_priority(1)
-	cam4.set_priority(0)
-	cam5.set_priority(4)
+func activate_camera(cam_index: int) -> void:
+		for i in range(cameras.size()):
+			if i == cam_index:
+				cameras[i].set_priority(4)  # Highest priority for the active camera
+			else:
+				cameras[i].set_priority(3 - i)  # Set decreasing priority for other cameras
+
+
+func _on_hall_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		activate_camera(0)  # First camera
+
+func _on_main_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		activate_camera(1)  # Second camera
+
+func _on_poster_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		activate_camera(2)  # Third camera
+
+func _on_window_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		activate_camera(3)  # Fourth camera
+
+func _on_kitchen_area_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		activate_camera(4)  # Fifth camera
 	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
 
-func _on_hall_area_body_entered(body: Node3D) -> void:
+func _on_door_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		cam1_on()
+		activate_camera(0)  # First camera
 
 
-func _on_main_area_body_entered(body: Node3D) -> void:
+func _on_book_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		cam2_on()
+		activate_camera(1)  # Second camera
 
 
-func _on_poster_area_body_entered(body: Node3D) -> void:
+func _on_wurli_area_body_entered(body: Node3D) -> void:
 	if body.is_in_group("player"):
-		cam3_on()
-
-
-func _on_window_area_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		cam4_on()
-
-
-func _on_kitchen_area_body_entered(body: Node3D) -> void:
-	if body.is_in_group("player"):
-		cam5_on()
+		activate_camera(2)  # Third camera
--- StartingOffice/scenechange.gd
+++ StartingOffice/scenechange.gd
@@ -17,8 +17,10 @@ func _on_firsthouse_button_pressed() -> void:
 
 
 func _on_secondhouse_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://SecondHouse/second_house.tscn")
 	pass # Replace with function body.
 
 
 func _on_thirdhouse_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://ThirdHouse/third_house.tscn")
 	pass # Replace with function body.
