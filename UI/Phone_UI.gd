28748646cee58bd70360cbc6b448e7ae6142bd81
arethali <arli@ucsd.edu>
2025-03-16 01:34:35 -0700

    More UI stuff

--- UI/Phone_UI.gd
+++ UI/Phone_UI.gd
@@ -1,13 +1,28 @@
 extends Node2D
 
-@onready var home = $PhoneHomeBack
-@onready var phone = $PhoneCallNumbers
-@onready var gallery = $PhoneGallery
-@onready var notes = $PhoneNotes
+#screen
+@onready var home = $HomeScreen
+@onready var phone = $PhoneScreen
+@onready var gallery = $GalleryScreen
+@onready var notes = $NotesScreen
 
-@onready var gallery_button = $Gallery
-@onready var notes_button = $Notes
-@onready var phone_button = $Phone
+#phone 
+@onready var phone_num = $PhoneScreen/PhoneNum
+@onready var phone_call = $PhoneScreen/PhoneCall
+
+#Gallery 
+@onready var gallery_list = $GalleryScreen/GalleryList
+@onready var gallery_close_pics = $GalleryScreen/ClosePics
+@onready var close_pic_1 = $"GalleryScreen/ClosePics/Close Pic1"
+@onready var close_pic_2 = $"GalleryScreen/ClosePics/Close Pic2"
+@onready var close_pic_3 = $"GalleryScreen/ClosePics/Close Pic3"
+@onready var close_pic_4 = $"GalleryScreen/ClosePics/Close Pic4"
+@onready var close_pic_5 = $"GalleryScreen/ClosePics/Close Pic5"
+@onready var close_pic_6 = $"GalleryScreen/ClosePics/Close Pic6"
+@onready var close_pic_7 = $"GalleryScreen/ClosePics/Close Pic7"
+@onready var gallery_buttons = $GalleryScreen/ClosePics/Buttons
+
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,41 +30,205 @@ func _ready():
 	phone.hide()
 	gallery.hide()
 	notes.hide()
-
+	gallery_close_pics.hide()
+	phone_call.hide()
 
 func _on_home_pressed():
 	home.show()
 	phone.hide()
 	gallery.hide()
 	notes.hide()
-	gallery_button.show()
-	notes_button.show()
-	phone_button.show()
 
 
 func _on_gallery_pressed():
 	home.hide()
 	phone.hide()
-	gallery.show()
 	notes.hide()
-	gallery_button.hide()
-	notes_button.hide()
-	phone_button.hide()
+	gallery.show()
+	gallery_list.show()
 
 func _on_notes_pressed():
 	home.hide()
 	phone.hide()
 	gallery.hide()
 	notes.show()
-	gallery_button.hide()
-	notes_button.hide()
-	phone_button.hide()
+
 
 func _on_phone_pressed():
 	home.hide()
-	phone.show()
 	gallery.hide()
 	notes.hide()
-	gallery_button.hide()
-	notes_button.hide()
-	phone_button.hide()
+	phone.show()
+	phone_num.show()
+
+#phone
+
+#gallery
+@onready var picArray = [close_pic_1, close_pic_2, close_pic_3, close_pic_4, close_pic_5, 
+close_pic_6, close_pic_7]
+@onready var picPlace = 0
+@onready var gallery_right = $GalleryScreen/ClosePics/Buttons/Right
+@onready var gallery_left = $GalleryScreen/ClosePics/Buttons/Left
+
+
+
+func _on_back_pressed():
+	gallery_close_pics.hide()
+	gallery_list.show()
+	picArray[picPlace].hide()
+	picPlace = 0
+	gallery_left.disabled = false
+	gallery_right.disabled = false
+
+
+func _on_left_pressed():
+	if picPlace == 1:
+		picPlace = 0
+		picArray[1].hide()
+		picArray[picPlace].show()
+		gallery_left.disabled = true
+		gallery_left.hide()
+	else:
+		gallery_right.disabled = false
+		#gallery_right.show()
+		picPlace -= 1
+		picArray[picPlace + 1].hide()
+	picArray[picPlace].show()
+	
+func _on_right_pressed():
+	if picPlace == 5:
+		picPlace = 6
+		picArray[5].hide()
+		picArray[picPlace].show()
+		gallery_right.disabled = true
+		gallery_right.hide()
+		picArray[picPlace-1].hide()
+	else:
+		gallery_left.disabled = false
+		#gallery_left.show()
+		picPlace += 1
+		picArray[picPlace-1].hide()
+	picArray[picPlace].show()
+
+
+func _on_pic_1_pressed():
+	picPlace = 0
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[0].show()
+	gallery_left.disabled = true
+	gallery_left.hide()
+
+
+
+func _on_pic_2_pressed():
+	picPlace = 1
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[1].show()
+
+
+func _on_pic_3_pressed():
+	picPlace = 2
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[2].show()
+	
+func _on_pic_4_pressed():
+	picPlace = 3
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[3].show()
+
+func _on_pic_5_pressed():
+	picPlace = 4
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[4].show()
+
+func _on_pic_6_pressed():
+	picPlace = 5
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[5].show()
+
+func _on_pic_7_pressed():
+	picPlace = 6
+	gallery_close_pics.show()
+	gallery_list.hide()
+	gallery_buttons.show()
+	picArray[6].show()
+	gallery_right.disabled = true
+	gallery_right.hide()
+
+
+func _on_right_hover_mouse_entered():
+	if picPlace != 6:
+		gallery_right.show()
+
+func _on_left_hover_mouse_entered():
+	if picPlace != 0:
+		gallery_left.show()
+
+func _on_right_hover_mouse_exited():
+	gallery_right.hide()
+
+func _on_left_hover_mouse_exited():
+	gallery_left.hide()
+
+#Phone 
+
+#Number input
+@onready var num_input = $PhoneScreen/PhoneNum/NumInput
+
+func inputNum(num: int):
+	if len(num_input.text) <  11:
+		num_input.text += str(num)
+		if len(num_input.text) == 3:
+			num_input.text += str("-")
+
+func _on_zero_pressed():
+	inputNum(0)
+
+func _on_one_pressed():
+	inputNum(1)
+
+func _on_two_pressed():
+	inputNum(2)
+
+func _on_three_pressed():
+	inputNum(3)
+	
+func _on_four_pressed():
+	inputNum(4)
+
+func _on_five_pressed():
+	inputNum(5)
+
+func _on_six_pressed():
+	inputNum(6)
+
+func _on_seven_pressed():
+	inputNum(7)
+
+func _on_eight_pressed():
+	inputNum(8)
+
+func _on_nine_pressed():
+	inputNum(9)
+
+func _on_delete_pressed():
+	if len(num_input.text) == 4:
+		num_input.text = num_input.text.erase(len(num_input.text)-2, 2)
+		print(num_input.text)
+	else:
+		num_input.text = num_input.text.erase(len(num_input.text)-1, 1)
+
+func _on_call_pressed():
+	print("Calling")
--- UI/computer_ui.gd
+++ UI/computer_ui.gd
@@ -1,18 +1,195 @@
 extends Node2D
 
+#screens
+@onready var login = $Login
+@onready var home = $Home
+@onready var mail = $Mail
+
+#Overlays
+@onready var Home_dropdown_overlay = $Home/Dropdown
+@onready var Mail_face_id_overlay = $"Mail/Face ID"
+@onready var Mail_new_mail_overlay = $"Mail/New Message"
+@onready var Mail_error_overlay = $Mail/Error
+
+#Home buttons
+@onready var Home_icon = $Home/Icon
+
+#Mail Screens
+@onready var Mail_inbox = $Mail/Inbox
+@onready var Mail_read = $Mail/Read
+
+
+#Mail buttons
+@onready var Mail_Exit = $"Mail/Mail exit"
+@onready var Mail_New_Message = $Mail/NewMessage
+@onready var Mail_Inbox_Message_1 = $Mail/Inbox/Message1
+@onready var Mail_Inbox_Message_2 = $Mail/Inbox/Message2
+@onready var Mail_Inbox_Message_3 = $Mail/Inbox/Message3
+@onready var Mail_Inbox_Message_4 = $Mail/Inbox/Message4
+@onready var Mail_Inbox_Message_5 = $Mail/Inbox/Message5
+@onready var Mail_Read_back = $Mail/Read/Back
+@onready var Mail_Read_forward = $Mail/Read/Forward
+@onready var Mail_Read_delete = $"Mail/Read/Mail Delete"
+@onready var Mail_Read_download = $Mail/Read/Download
+@onready var Mail_Read_lock = $Mail/Read/Lock
+
+
+@onready var Mail_button_array = [Mail_Exit, Mail_New_Message, Mail_Inbox_Message_1, Mail_Inbox_Message_2, Mail_Inbox_Message_3,
+Mail_Inbox_Message_4, Mail_Inbox_Message_5, Mail_Read_back, Mail_Read_forward, Mail_Read_delete, Mail_Read_download, Mail_Read_lock]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Home/Dropdown.hide()
-	pass # Replace with function body.
+	login.show()
+	home.hide()
+	mail.hide()
+	Home_dropdown_overlay.hide()
+	Mail_face_id_overlay.hide()
+	Mail_new_mail_overlay.hide()
+	Mail_error_overlay.hide()
+
+#Login
+func _on_enter_pressed():
+	login.hide()
+	home.show()
+
+func _on_hint_pressed():
+	$Login/HintText.show()
+	await get_tree().create_timer(1.5).timeout
+	$Login/HintText.hide()
+
+func _on_sleep_pressed():
+	get_tree().quit()
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _on_restart_pressed():
+	get_tree().quit()
 
+
+func _on_shutdown_pressed():
+	get_tree().quit()
+
+
+#Home
 func _on_icon_toggled(toggled_on):
 	if toggled_on == true:
-		$Home/Dropdown.show()
+		Home_dropdown_overlay.show()
 	else:
-		$Home/Dropdown.hide()
+		Home_dropdown_overlay.hide()
+
+func _on_dropdown_1_pressed():
+	#Log out
+	Home_dropdown_overlay.hide()
+	Home_icon.set_pressed(false)
+	login.show()
+	home.hide()
+
+func _on_dropdown_2_pressed():
+	get_tree().quit()
+
+
+func _on_dropdown_3_pressed():
+	get_tree().quit()
+
+
+func _on_dropdown_4_pressed():
+	get_tree().quit()
+
+
+func _on_dropdown_5_pressed():
+	get_tree().quit()
+
+func _on_mail_pressed():
+	Home_dropdown_overlay.hide()
+	Home_icon.set_pressed(false)
+	mail.show()
+	Mail_inbox.show()
+	Mail_read.hide()
+	home.hide()
+
+#Mail
+
+#inbox
+func _on_mail_exit_pressed():
+	mail.hide()
+	Mail_inbox.hide()
+	Mail_error_overlay.hide()
+	Mail_new_mail_overlay.hide()
+	home.show()
+
+func _on_message_1_pressed():
+	faceScan()
+
+func _on_message_2_pressed():
+	faceScan()
+
+func _on_message_3_pressed():
+	faceScan()
+
+func _on_message_4_pressed():
+	Mail_inbox.hide()
+	Mail_read.show()
+
+func _on_message_5_pressed():
+	Mail_inbox.hide()
+	Mail_read.show()
+
+#new message
+func _on_new_message_pressed():
+	disableMailButtons()
+	Mail_new_mail_overlay.show()
+
+func _on_confirm_pressed():
+	Mail_error_overlay.hide()
+	enableMailButtons()
+
+func _on_exit_pressed():
+	Mail_new_mail_overlay.hide()
+	enableMailButtons()
+
+func _on_error_exit_pressed():
+	Mail_error_overlay.hide()
+	
+	
+func _on_send_pressed():
+	disableMailButtons()
+	Mail_error_overlay.show()
+
+func _on_delete_pressed():
+	disableMailButtons()
+	#change to confirm delete
+	Mail_error_overlay.show()
+
+#read
+
+func _on_back_pressed():
+	Mail_inbox.show()
+	Mail_read.hide()
+
+func _on_mail_delete_pressed():
+	faceScan()
+
+func _on_download_pressed():
+	faceScan()
+
+func _on_lock_pressed():
+	faceScan()
+
+func _on_forward_pressed():
+	faceScan()
+
+#local functions
+func disableMailButtons():
+	for i in Mail_button_array:
+		i.mouse_filter = 2 
+		
+func enableMailButtons():
+	for i in Mail_button_array:
+		i.mouse_filter = 0
+
+func faceScan():
+	disableMailButtons()
+	Mail_face_id_overlay.show()
+	$"Mail/Face ID/FaceID Anim".play("Face-ID")
+	await $"Mail/Face ID/FaceID Anim".animation_finished
+	Mail_face_id_overlay.hide()
+	enableMailButtons()
