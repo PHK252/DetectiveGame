aaac84455a59015ba7030816750d5e8b9db2a8d7
arethali <arli@ucsd.edu>
2025-04-05 22:11:31 -0700

    Reapply "Added Assets in Environment + Cams., started on refining interactions"
    This reverts commit ee08d66f12760a1863443ba46c4ab3cc618b5a42.

---
+++ SecondHouse/employee_info.gd
@@ -0,0 +1,11 @@
+extends "res://Global Scripts/Framework_Interact_To_Look.gd"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
--- SecondHouse/juniper_mouse_active.gd
+++ SecondHouse/juniper_mouse_active.gd
@@ -26,3 +26,11 @@ func _on_resume_mouse_entered():
 
 func _on_resume_mouse_exited():
 	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
+
+
+func _on_employee_info_mouse_entered():
+	Input.set_default_cursor_shape(Input.CURSOR_POINTING_HAND)
+
+
+func _on_employee_info_mouse_exited():
+	Input.set_default_cursor_shape(Input.CURSOR_ARROW)
