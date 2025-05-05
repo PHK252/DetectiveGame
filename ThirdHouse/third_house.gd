7d13523741880ddd72223f581d889b4be1004257
arethali <arli@ucsd.edu>
2025-05-05 16:03:36 -0700

    About to update

--- ThirdHouse/third_house.gd
+++ ThirdHouse/third_house.gd
@@ -3,6 +3,9 @@ extends Node3D
 func _ready():
 	GlobalVars.current_level = "Quincy"
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+	#var uid = ResourceUID.text_to_id("uid://b5qu46pybr7wc")
+	#print(ResourceUID.get_id_path(uid))
+	#print(ResourceUID.has_id(uid))
 
 func _process(delta):
 	if Input.is_action_just_pressed("Quit"):
