c3dcc54b3d7318dc143a405e44d9230eadfc9948
arethali <arli@ucsd.edu>
2025-08-10 09:59:36 -0700

    tiny update

--- FirstHouse/debug_label.gd
+++ FirstHouse/debug_label.gd
@@ -7,9 +7,9 @@ extends Label
 @export var cam_array : Array[PhantomCamera3D] = []
 
 func _process(delta):
-	for i in cam_array:
-		if i.is_active():
-			debug_var = i.name
-	#debug_var = timer.get_time_left()
+	#for i in cam_array:
+		#if i.is_active():
+			#debug_var = i.name
+	debug_var = timer.get_time_left()
 	#debug_var_1 = Dialogic.VAR.get_variable("Asked Questions.Micah_cab")
 	label.text = str(debug_var)
