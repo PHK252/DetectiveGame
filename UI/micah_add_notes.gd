2dc524bc43396fa3d454781d6f1d3b47f4cd9ad3
arethali <arli@ucsd.edu>
2025-08-09 19:21:49 -0700

    Phone notes system backbone done

---
+++ UI/micah_add_notes.gd
@@ -0,0 +1,19 @@
+extends Control
+
+var text = "REVER: The town is a quarantine site for an experiment to combine human DNA with animal DNA."
+var text2 = "hwllooo"
+
+@export var text_label : RichTextLabel
+
+func _ready():
+	_add_note(text)
+
+func _add_note(text : String):
+	if text_label.text == "ADD NOTES HERE":
+		text_label.text = ""
+	
+	if text_label.text == "":
+		text_label.text = text
+	else:
+		text_label.text += "\n" + text
+	pass
---
+++ UI/notes_screen.gd
@@ -0,0 +1,57 @@
+extends Control
+
+#screens
+@export var home: Control
+@export var case: Control
+@export var micah: Control
+@export var juniper: Control
+@export var quincy: Control
+
+#button behavior
+func _on_visibility_changed():
+	if visible == true:
+		set_default()
+
+func set_default():
+	micah.hide() 
+	juniper.hide()
+	quincy.hide()
+	case.hide()
+	home.show()
+
+
+func _on_notes_pressed():
+	case.show()
+	home.hide()
+
+
+func _on_victim_1_pressed():
+	case.hide()
+	micah.show()
+	
+func _on_victim_2_pressed():
+	case.hide()
+	juniper.show()
+
+func _on_victim_3_pressed():
+	case.hide()
+	quincy.show()
+
+func _on_back_pressed():
+	case.hide()
+	home.show()
+
+
+func _on_micah_back_pressed():
+	micah.hide()
+	case.show()
+
+
+func _on_Juniper_back_pressed():
+	juniper.hide()
+	case.show()
+
+
+func _on_quincy_back_pressed():
+	quincy.hide()
+	case.show()
