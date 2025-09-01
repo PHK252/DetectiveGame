f7086308ad1b0c15de4249efc7293512e34b717e
PHK252 <pranavkurup26@gmail.com>
2025-09-01 12:59:53 -0400

    chocolate anim interrogation working

--- Cutscenes/InterrogationAnimations.gd
+++ Cutscenes/InterrogationAnimations.gd
@@ -15,6 +15,9 @@ extends Node
 @export var DoorAnim : AnimationPlayer
 @export var soundplayer : AnimationPlayer
 
+#chocolate show
+@export var chocolate_animation : AnimationPlayer
+
 func _ready() -> void:
 	cam_anims.play("IntroAnimation")
 	await get_tree().create_timer(10).timeout
@@ -31,3 +34,6 @@ func _process(delta: float) -> void:
 		S_anim["parameters/conditions/release_skylar"] = true
 		T_anim["parameters/conditions/release_skylar"] = true
 		cam_anims.play("OutroAnimation")
+	
+	if Input.is_action_just_pressed("meeting_done"):
+		cam_anims.play("ChocoCam")
