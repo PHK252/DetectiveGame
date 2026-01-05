fb93e5ea678713c25b700a9b8e29256e29fd3fbf
PHK252 <pranavkurup26@gmail.com>
2026-01-05 11:46:19 -0500

    sound fixes junipers

--- UI/multipage_object_look.gd
+++ UI/multipage_object_look.gd
@@ -3,6 +3,10 @@ extends CanvasLayer
 @onready var pages = [$Page1, $Page2, $Page3, $Page4, $Page5, $Page6, $Page7]
 @onready var pCount = 0
 @export var pageCount = 7
+
+signal paper_sound
+
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass
@@ -15,7 +19,7 @@ func _process(delta):
 
 func _on_forward_pressed():
 	#turnPageForward(5)
-	
+	emit_signal("paper_sound")
 	if pCount == pageCount - 1:
 		pCount = 0
 		pages[pageCount-1].hide()
@@ -26,6 +30,7 @@ func _on_forward_pressed():
 	#print(pCount)
 
 func _on_back_pressed():
+	emit_signal("paper_sound")
 	if pCount == 0:
 		pCount = pageCount - 1
 		pages[0].hide()
