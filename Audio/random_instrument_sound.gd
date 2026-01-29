d1ccacf8f5cbe9b0a8830e2f75f772b22618b353
arethali <arli@ucsd.edu>
2026-01-28 19:00:19 -0800

    nearest is the nearest near by the nearest near + random audio setup

---
+++ Audio/random_instrument_sound.gd
@@ -0,0 +1,22 @@
+extends Node3D
+
+@export var player : AudioStreamPlayer
+@export var stream_array : Array[String]
+var playing := false
+
+func _on_guitar_interact_interacted(interactor):
+	if !playing:
+		var arr_size = stream_array.size()
+		if arr_size != 0:
+			playing = true
+			var stream_num = randi_range(0, arr_size-1)
+			var curr_stream = load(stream_array[stream_num])
+			player.stream = curr_stream
+			player.play()
+		else:
+			print_debug("no streams :(")
+			return
+
+
+func _on_guitar_sounds_finished():
+	playing = false
--- dream_stuff/tile_check.gd
+++ dream_stuff/tile_check.gd
@@ -5,8 +5,17 @@ var tile_matrix = [[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,0,0,0,0],[0,0,
 var day_1_matrix = [[0,0,0,0,0,0],[0,0,1,1,0,0],[0,0,1,1,0,0],[0,0,1,1,0,0],[0,0,0,0,0,0]]
 var day_2_matrix = [[0,0,1,1,0,0],[0,1,0,0,1,0],[1,0,0,0,0,1],[0,1,0,0,1,0],[0,0,1,1,0,0]]
 
+@export var clue : MeshInstance3D
+@export var text_day_1 : String
+@export var text_day_2 : String
 @export var player : CharacterBody3D
 
+func _ready():
+	if GlobalVars.day == 1:
+		clue.get_active_material(0).albedo_texture = load(text_day_1)
+	else:
+		clue.get_active_material(0).albedo_texture = load(text_day_2)
+	#get_active_material(0).set_shader_parameter("texture_albedo", load(texture))
 
 func _on_tile_change(tile_x_num, tile_y_num, status):
 	tile_matrix[tile_x_num-1][tile_y_num-1] = status
