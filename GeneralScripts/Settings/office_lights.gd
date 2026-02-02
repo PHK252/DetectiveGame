e28be3fd5cf6b70785f2205d448bef0e3a9b4b9a
PHK252 <pranavkurup26@gmail.com>
2026-02-02 11:54:38 -0500

    twilight light + testing

--- GeneralScripts/Settings/office_lights.gd
+++ GeneralScripts/Settings/office_lights.gd
@@ -16,30 +16,68 @@ extends Node
 @export var night_spot_03 : SpotLight3D
 @export var night_spot_04 : SpotLight3D
 
+#twilight
+@export var t_01 : SpotLight3D
+@export var t_02 : SpotLight3D
+
 func _ready() -> void:
-	#daylighting
-	world_env.environment.set_bg_energy_multiplier(2.07)
-	toggle_nightlight(false)
-	toggle_daylight(true)
-	#
-	#nightlighting
-	#world_env.environment.set_bg_energy_multiplier(0.0)
+	pass
+	##daylighting
+	#toggle_nightlight(false)
+	#toggle_twilight(false)
+	#toggle_daylight(true)
+#
+	##nightlighting
 	#toggle_daylight(false)
+	#toggle_twilight(false)
 	#toggle_nightlight(true)
-	#
-	#twilight lighting
+#
+	##twilight lighting
+	#toggle_daylight(false)
+	#toggle_nightlight(false)
+	#toggle_twilight(true)
+
+func _process(delta: float) -> void:
+	#toggles for testing
+	if Input.is_action_just_pressed("call"):
+		toggle_nightlight(false)
+		toggle_twilight(false)
+		toggle_daylight(true)
 	
-	#alternate daylight
-	
+	if Input.is_action_just_pressed("meeting_done"):
+		toggle_daylight(false)
+		toggle_twilight(false)
+		toggle_nightlight(true)
+		
+	if Input.is_action_just_pressed("interact"):
+		toggle_daylight(false)
+		toggle_nightlight(false)
+		toggle_twilight(true)
+
+
 func toggle_daylight(toggle : bool):
+	if toggle:
+		world_env.environment.set_bg_energy_multiplier(2.07)
+		world_env.environment.sky.sky_material.set_sky_top_color(Color(0.385, 0.454, 0.55, 1))
 	lamp_light.visible = toggle
 	spot_01.visible = toggle
 	spot_02.visible = toggle
 	directional.visible = toggle
 
 func toggle_nightlight(toggle : bool):
-	night_spot_01 .visible = toggle
-	night_spot_02 .visible = toggle
-	night_spot_03 .visible = toggle
-	night_spot_04 .visible = toggle
+	if toggle:
+		world_env.environment.set_bg_energy_multiplier(0.0)
+		world_env.environment.sky.sky_material.set_sky_top_color(Color(0.385, 0.454, 0.55, 1))
+	night_spot_01.visible = toggle
+	night_spot_02.visible = toggle
+	night_spot_03.visible = toggle
+	night_spot_04.visible = toggle
+	
+func toggle_twilight(toggle : bool):
+	if toggle:
+		world_env.environment.set_bg_energy_multiplier(1.88)
+		world_env.environment.sky.sky_material.set_sky_top_color(Color(0.468, 0.458, 0.458, 1))
+	lamp_light.visible = toggle
+	t_01.visible = toggle
+	t_02.visible = toggle
 	
