7abde4ed7024c65a5959a395016d0d7ff114290f
arethali <arli@ucsd.edu>
2026-03-25 01:02:49 -0700

    Revised Font and font size + fixed sound interact

--- Audio/random_instrument_sound.gd
+++ Audio/random_instrument_sound.gd
@@ -2,17 +2,16 @@ extends Node3D
 
 @export var dalton : CharacterBody3D
 @export var player : AudioStreamPlayer
-@export var sounds_folder : String
+#@export var sounds_folder : String
 @export var alert : Sprite3D
-var stream_array : Array[String]
+@export var stream_array : Array[String]
+var play_array: Array[AudioStream]
 var playing := false
 var bus_ind = AudioServer.get_bus_index("Music")
 var starting_db = AudioServer.get_bus_volume_db(bus_ind)
 
 func _ready():
-	if sounds_folder:
-		get_sounds_from_folder(sounds_folder)
-		print(stream_array)
+	_load_streams(stream_array)
 
 func _on_guitar_interact_interacted(interactor):
 	if !playing and !GlobalVars.in_dialogue:
@@ -20,10 +19,11 @@ func _on_guitar_interact_interacted(interactor):
 		if arr_size != 0:
 			playing = true
 			var stream_num = randi_range(0, arr_size-1)
-			var curr_stream = load(stream_array[stream_num])
+			var curr_stream = play_array[stream_num]
 			side_chain("Music", -15, .5)
 			await get_tree().create_timer(.5).timeout
 			player.stream = curr_stream
+			GlobalVars.in_interaction = "playing"
 			alert.hide()
 			dalton.stop_player()
 			player.play()
@@ -44,15 +44,20 @@ func _on_guitar_sounds_finished():
 	if !GlobalVars.in_dialogue:
 		alert.show()
 		dalton.start_player()
+	GlobalVars.in_interaction = ""
 	playing = false
 	side_chain("Music", starting_db, .25)
 	await get_tree().create_timer(.5).timeout
 
-func get_sounds_from_folder(folder : String):
-	var dir_access := DirAccess.open(folder)
-	if dir_access:
-		for file in dir_access.get_files():
-			if file.get_extension().to_lower() == "wav":
-				stream_array.append(folder.path_join(file))
-	else:
-		print_debug("Can not open folder : ", folder)
+
+func _load_streams(arr: Array[String]):
+	for stream in arr.size():
+		play_array.append(load(arr[stream]))
+#func get_sounds_from_folder(folder : String):
+	#var dir_access := DirAccess.open(folder)
+	#if dir_access:
+		#for file in dir_access.get_files():
+			#if file.get_extension().to_lower() == "wav":
+				#stream_array.append(folder.path_join(file))
+	#else:
+		#print_debug("Can not open folder : ", folder)
